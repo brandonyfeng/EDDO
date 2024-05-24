2e266461d4ac16d0c314781d992daaa54e32b8df
brandonyfeng <fengyushan1002@gmail.com>
2024-05-24 11:33:20 -0400

    Initial commit

--- scripts/run_JWST_test.sh
+++ scripts/run_JWST_test.sh
@@ -2,5 +2,5 @@ CUDA_VISIBLE_DEVICES=1 python run_JWST.py \
     --iters 1000 --scene_name test/JWST --num_t 1 \
     --lr 1e-11 \
     --log_progress \
-    --error_ratio 0.05 --contrast_mult 1 --contrast_exp -8 \
+    --error_ratio 0.01 --contrast_mult 1 --contrast_exp -8 \
     --photon_noise 0.0001
